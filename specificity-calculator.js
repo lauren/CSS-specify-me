664be9302e2580af7b4c92241472a1d071812b9e
lauren <lauren.sperber@gmail.com>
2013-06-17 13:38:20 -0400

    break out validatePseudos function

--- specificity-calculator.js
+++ specificity-calculator.js
@@ -16,7 +16,7 @@
     }
   };
 
-  // takes an array of selectors and resturns an array with the specificity of each
+  // takes an array of selectors and returns an array with the specificity of each
   var getSpecificities = function (array) {
     var specificities = [],
         specificityGetter = function (thisArray) {
@@ -33,6 +33,8 @@
     return specificityGetter(array);
   };
 
+  // takes a single selector string, passes to parser, then iterates over resulting 
+  // array of selector objects summing their specificities
   var calculateSpecificity = function (selectors) {
     var selectorObjectArray = parseSelectors(selectors),
         specificity = [0,0,0],
@@ -52,7 +54,8 @@
     return calculateArraySpecificity(selectorObjectArray);
   };
 
-  // requires two specificity arrays of the same length
+  // requires two specificity arrays of the same length. luckily, specificity arrays are
+  // always the same length!
   var sumSpecificityArrays = function (array1, array2) {
     var summedArray = [],
         sumArrays = function (xs, ys) {
@@ -91,36 +94,28 @@
   var parseSelectors = function (selectors) {
     selectors = selectors.split(/(\s|\*|\+|\~|\<)/);
     // change each selector into object with ID/class/node property based
-    // on first character
+    // on first character, then parse out additional classes or IDs if needed.
     // we're going from ["node.class", "node#id", "#id.class.class", ".class.class"] to
-    // [{node: 'node.class'}, {node: 'node#id'}, {id: #id.class.class}, {classes: ["class.class"]}]
+    // [{node: 'node', classes: ['.class'}, {node: 'node', id: '#id'}, 
+    // {id: '#id', classes: ['.class', '.class']}, {classes: ['class', '.class']}]
     for (var i = 0; i < selectors.length; i++) {
       switch(selectors[i].charAt(0)) {
-      case "*": case "+": case "~": case "<": case ">": case "": case " ":
-        selectors[i] = {wildcardOrCombinator: selectors[i]};
-        break;
-      case "#":
-        selectors[i] = {id: selectors[i]};
-        findClassesInObject(selectors[i]);
-        break;
-      case ".":
-        selectors[i] = {classes: findClasses(selectors[i])};
-        break;
-      default:
-        selectors[i] = {node: selectors[i]};
-        if (/#/.test(selectors[i].node)) {
-          selectors[i] = findNodeIds(selectors[i].node);
-        }
-        findClassesInObject(selectors[i]);
+        case "*": case "+": case "~": case "<": case ">": case "": case " ":
+          selectors[i] = {wildcardOrCombinator: selectors[i]};
+          break;
+        case "#":
+          selectors[i] = {id: selectors[i]};
+          break;
+        case ".":
+          selectors[i] = {classes: findClasses(selectors[i])};
+          break;
+        default:
+          selectors[i] = {node: selectors[i]};
+          if (/#/.test(selectors[i].node)) {
+            selectors[i] = findNodeIds(selectors[i].node);
+          }
+      findClassesInObject(selectors[i]);
       }
-    };
-      
-    // find additional classes and IDs in the main selector to flush out the object
-    // we're going from 
-    // [{node: 'node.class'}, {node: 'node#id'}, {id: #id.class.class}, {classes: ["class.class"]}] to
-    // [{node: "node", classes: [".class"]}, {node: "node", id: "#id"}, 
-    // {id: "#id", classes: [".class", ".class"]}, {classes: [".class", ".class"]}]
-    for (var i = 0; i < selectors.length; i++) {
       for (type in selectors[i]) {
         if (typeof selectors[i][type] === "object") {
           for (var j = 0; j < selectors[i][type].length; j++) {
@@ -168,22 +163,78 @@
           }
         }
       }
-      if (selectors[i].pseudos) {
-        selectors[i].pseudoClasses = []
-        for (var j = 0; j < selectors[i].pseudos.length; j++) {
-          if (/(\[|\])/.test(selectors[i].pseudos[j])) {
-            selectors[i].attributes = findAttributes(selectors[i].pseudos[j]);
-            selectors[i].pseudos[j] = selectors[i].pseudos[j].split("[")[0];
-          }
-          if (inArray(pseudoElements, ":" + selectors[i].pseudos[j])) { 
-            selectors[i].pseudoElement = ":" + selectors[i].pseudos[j];
-          } else if (inArray(pseudoClasses, selectors[i].pseudos[j])) {
-            selectors[i].pseudoClasses.push(selectors[i].pseudos[j]);
-          }
-        }
-        delete selectors[i].pseudos;
-      }
+      validatePseudos(selectors[i]);
     };
+      
+    // find additional classes and IDs in the main selector to flush out the object
+    // we're going from 
+    // [{node: 'node.class'}, {node: 'node#id'}, {id: #id.class.class}, {classes: ["class.class"]}] to
+    // [{node: "node", classes: [".class"]}, {node: "node", id: "#id"}, 
+    // {id: "#id", classes: [".class", ".class"]}, {classes: [".class", ".class"]}]
+    // for (var i = 0; i < selectors.length; i++) {
+      // for (type in selectors[i]) {
+      //   if (typeof selectors[i][type] === "object") {
+      //     for (var j = 0; j < selectors[i][type].length; j++) {
+      //       if (/::/.test(selectors[i][type][j])) {
+      //         selectors[i].pseudoElement = findPseudoElements(selectors[i][type][j]);
+      //         selectors[i][type][j] = selectors[i][type][j].split("::")[0];
+      //         if (/:/.test(selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length))) {
+      //           selectors[i].pseudos = findPseudos(selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length));
+      //           selectors[i].pseudoElement = selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length).split(":")[0];
+      //         }
+      //         if (/(\[|\])/.test(selectors[i].pseudoElement)) {
+      //           selectors[i].attributes = findAttributes(selectors[i].pseudoElement);
+      //           selectors[i].pseudoElement = selectors[i].pseudoElement.split("[")[0];
+      //         }
+      //       }
+      //       if (/:/.test(selectors[i][type][j])) {
+      //         selectors[i].pseudos = findPseudos(selectors[i][type][j]);
+      //         selectors[i][type][j] = selectors[i][type][j].split(":")[0];
+      //       }
+      //       if (/(\[|\])/.test(selectors[i][type][j])) {
+      //         selectors[i].attributes = findAttributes(selectors[i][type][j]);
+      //         selectors[i][type][j] = selectors[i][type][j].split("[")[0];
+      //       }
+      //     }
+      //   } else {
+      //     if (/::/.test(selectors[i][type])) {
+      //       selectors[i].pseudoElement = findPseudoElements(selectors[i][type]);
+      //       selectors[i][type] = selectors[i][type].split("::")[0];
+      //       if (/:/.test(selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length))) {
+      //         selectors[i].pseudos = findPseudos(selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length));
+      //         selectors[i].pseudoElement = selectors[i].pseudoElement.slice(2,selectors[i].pseudoElement.length).split(":")[0];
+      //       }
+      //       if (/(\[|\])/.test(selectors[i].pseudoElement)) {
+      //         selectors[i].attributes = findAttributes(selectors[i].pseudoElement);
+      //         selectors[i].pseudoElement = selectors[i].pseudoElement.split("[")[0];
+      //       }
+      //     }
+      //     if (/:/.test(selectors[i][type])) {
+      //       selectors[i].pseudos = findPseudos(selectors[i][type]);
+      //       selectors[i][type] = selectors[i][type].split(":")[0];
+      //     }
+      //     if (/(\[|\])/.test(selectors[i][type])) {
+      //       selectors[i].attributes = findAttributes(selectors[i][type]);
+      //       selectors[i][type] = selectors[i][type].split("[")[0];
+      //     }
+      //   }
+      // }
+      // if (selectors[i].pseudos) {
+      //   selectors[i].pseudoClasses = []
+      //   for (var j = 0; j < selectors[i].pseudos.length; j++) {
+      //     if (/(\[|\])/.test(selectors[i].pseudos[j])) {
+      //       selectors[i].attributes = findAttributes(selectors[i].pseudos[j]);
+      //       selectors[i].pseudos[j] = selectors[i].pseudos[j].split("[")[0];
+      //     }
+      //     if (inArray(pseudoElements, ":" + selectors[i].pseudos[j])) { 
+      //       selectors[i].pseudoElement = ":" + selectors[i].pseudos[j];
+      //     } else if (inArray(pseudoClasses, selectors[i].pseudos[j])) {
+      //       selectors[i].pseudoClasses.push(selectors[i].pseudos[j]);
+      //     }
+      //   }
+      //   delete selectors[i].pseudos;
+      // }
+    // };
     return selectors;
   }
 
@@ -238,6 +289,26 @@
     }
     return pseudos;
   };
+
+  // takes a selector object. parses attributes out of pseudos if necessary, then checks 
+  // each pseudo to categorize it as a pseudo-element or pseudo-class
+  var validatePseudos = function (object) {
+    if (object.pseudos) {
+      object.pseudoClasses = []
+      for (var i = 0; i < object.pseudos.length; i++) {
+        if (/(\[|\])/.test(object.pseudos[i])) {
+          object.attributes = findAttributes(object.pseudos[i]);
+          object.pseudos[i] = object.pseudos[i].split("[")[0];
+        }
+        if (inArray(pseudoElements, ":" + object.pseudos[i])) { 
+          object.pseudoElement = ":" + object.pseudos[i];
+        } else if (inArray(pseudoClasses, object.pseudos[i])) {
+          object.pseudoClasses.push(object.pseudos[i]);
+        }
+      }
+      delete object.pseudos;
+    }
+  }
 
   // takes a string like "node[attribute][attribute]" and returns an array like 
   // [attribute, attribute]
